d7df58d0127ae9b4b9d396d58d372299fe7b29ae
fabian fuentes <cffuentes@tec.mx>
2026-04-21 13:49:57 -0600

    calibrate energy demand firs vers

--- ssp_modeling/output_postprocessing/postprocessing_libya.r
+++ ssp_modeling/output_postprocessing/postprocessing_libya.r
@@ -13,7 +13,7 @@ rm(list=ls())
 
 #ouputfile
 
-run <- 'sisepuede_results_sisepuede_run_2026-04-20T19;52;08.574229'
+run <- 'sisepuede_results_sisepuede_run_2026-04-21T13;37;46.729854'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 output.file <- paste0(run, "_WIDE_INPUTS_OUTPUTS.csv")
