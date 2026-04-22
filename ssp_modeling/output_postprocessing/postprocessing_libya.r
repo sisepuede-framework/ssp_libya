bdb4628bb214ca27a7636e23b9ae20ca15e214dd
fabian fuentes <cffuentes@tec.mx>
2026-04-21 19:13:13 -0600

    water elec demand

--- ssp_modeling/output_postprocessing/postprocessing_libya.r
+++ ssp_modeling/output_postprocessing/postprocessing_libya.r
@@ -13,7 +13,7 @@ rm(list=ls())
 
 #ouputfile
 
-run <- 'sisepuede_results_sisepuede_run_2026-04-21T13;37;46.729854'
+run <- 'sisepuede_results_sisepuede_run_2026-04-21T19;02;38.130743'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 output.file <- paste0(run, "_WIDE_INPUTS_OUTPUTS.csv")
