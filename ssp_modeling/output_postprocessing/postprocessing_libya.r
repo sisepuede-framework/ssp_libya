bf474fb2466ae3961f80ef02ef1ee34b0d11c0fa
fabian fuentes <cffuentes@tec.mx>
2026-04-22 18:52:47 -0600

    fix transport target

--- ssp_modeling/output_postprocessing/postprocessing_libya.r
+++ ssp_modeling/output_postprocessing/postprocessing_libya.r
@@ -13,7 +13,7 @@ rm(list=ls())
 
 #ouputfile
 
-run <- 'sisepuede_results_sisepuede_run_2026-04-22T17;11;29.342482'
+run <- 'sisepuede_results_sisepuede_run_2026-04-22T18;42;58.828991'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 output.file <- paste0(run, "_WIDE_INPUTS_OUTPUTS.csv")
