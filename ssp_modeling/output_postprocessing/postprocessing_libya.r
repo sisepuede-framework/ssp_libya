33f2aaf68d34ec1d81dea8568d8cdc7119ae4c6b
fabian fuentes <cffuentes@tec.mx>
2026-04-23 12:04:21 -0600

    change transformations transport

--- ssp_modeling/output_postprocessing/postprocessing_libya.r
+++ ssp_modeling/output_postprocessing/postprocessing_libya.r
@@ -13,7 +13,7 @@ rm(list=ls())
 
 #ouputfile
 
-run <- 'sisepuede_results_sisepuede_run_2026-04-22T19;28;43.391492'
+run <- 'sisepuede_results_sisepuede_run_2026-04-23T11;51;12.309697'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 output.file <- paste0(run, "_WIDE_INPUTS_OUTPUTS.csv")
